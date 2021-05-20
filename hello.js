9e58dd0b8c9a8049cc3363424c929b7147819647
Boseme Mukete <bosememukete@bosemes-mbp.myfiosgateway.com>
2021-05-20 17:32:40 -0400

    first commit

---
+++ hello.js
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+<script>
+function myFunction() {
+  document.getElementById("demo").innerHTML = "Hello World!";
+}
+</script>
+</head>
+
+<body>
+
+
+<button onclick="myFunction()">Click me</button>
+
+<p id="demo"></p>
+
+</body>
+</html>
+
+
